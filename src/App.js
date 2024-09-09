0cf18df00c7f823f943daaf24b542cf4df197def
Raghib <rahgib@gmail.com>
2024-09-09 09:50:53 +0530

    setting up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="font-bold text-green-800 ">
+      <header className="App-header">
+        Namaste react lets make netflix gpt
+      </header>
+    </div>
+  );
+}
+
+export default App;
